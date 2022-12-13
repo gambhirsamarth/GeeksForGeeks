7a680ffade370dafb1996729d475bd8bb677cd0d
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2022-12-13 21:29:56 +0530

    Create solution.cpp

---
+++ Sum of first n terms/solution.cpp
@@ -0,0 +1,14 @@
+// User function template for C++
+
+class Solution {
+  public:
+long long sumOfSeries(long long N) {
+
+        // code here
+
+        long long sum=N * (N+1)/2;
+
+        return sum*sum;
+
+    }
+};
