af782478f5cf4c7eba112659cacc4bb7f8f8abf1
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2022-12-14 20:59:07 +0530

    Create solution.cpp

---
+++ Bubble Sort/solution.cpp
@@ -0,0 +1,19 @@
+//User function Template for C++
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        // Your code here  
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+};
