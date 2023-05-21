35e410ee86b367fdf357a41d46c9ac0d5bad4c3c
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-05-21 22:39:38 +0530

    Create Solution.java

---
+++ Search in Linked List/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        Node temp = head;
+        
+        while(temp!=null){
+            if(temp.data == key) return true;
+            temp=temp.next;
+        }
+        
+        return false;
+    }
+}
